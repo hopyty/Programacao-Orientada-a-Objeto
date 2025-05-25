ce6b074e0409fa9fc63979d3b0fb16f58928de5a
IGOR GABRIEL <165973923+hopyty@users.noreply.github.com>
2025-05-25 00:44:05 -0300

    Update Roteiro 0006 Exercicio 3.cs

--- Roteiro 0006 Exercicio 3.cs
+++ Roteiro 0006 Exercicio 3.cs
@@ -1,1 +1,19 @@
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número inteiro: ");
+
+        try
+        {
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Você digitou: {numero}");
+        }
+        catch
+        {
+            Console.WriteLine("Valor inválido. Por favor, digite um número inteiro.");
+        }
+    }
+}
