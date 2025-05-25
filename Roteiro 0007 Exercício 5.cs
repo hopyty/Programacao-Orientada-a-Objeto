3ce702c16a8c3ab9ab765f6d6eab2843b336026c
IGOR GABRIEL <165973923+hopyty@users.noreply.github.com>
2025-05-25 00:54:46 -0300

    Update Roteiro 0007 Exercício 5.cs

--- Roteiro 0007 Exercício 5.cs
+++ Roteiro 0007 Exercício 5.cs
@@ -1,1 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite uma palavra: ");
+        string palavra = Console.ReadLine();
+
+        Stack<char> pilha = new Stack<char>();
+
+        foreach (char c in palavra)
+        {
+            pilha.Push(c);
+        }
+
+        string palavraInvertida = "";
+        while (pilha.Count > 0)
+        {
+            palavraInvertida += pilha.Pop();
+        }
+
+        Console.WriteLine($"Palavra invertida: {palavraInvertida}");
+    }
+}
 
